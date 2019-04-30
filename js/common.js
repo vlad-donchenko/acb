b43260a3984f847df9a38f32471007fd9bb7f39c
Vlad Donchenko <2pi14b.donchenko@gmail.com>
2019-04-30 14:24:02 +0300

    Лендинг готов - адаптив и моб версия рус и укр версия

---
+++ js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.popap_button').magnificPopup({
+    type:'inline',
+    midClick: true,
+  });
+});
+
+$("[data-mask='callback-catalog-phone']").mask("+380 9 9  9 9 9  9 9  9 9");
